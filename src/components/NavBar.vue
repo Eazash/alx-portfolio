1171a0840790873cc7ce83d4b3cb3fa95a97bcb4
Ezira Ashenafi <eazash22@gmail.com>
2023-08-14 14:40:30 +0300

    fix: return subtle blur to nav

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,8 +2,8 @@
   import NavLink from './NavLink.vue';
 </script>
 <template>
-  <nav class="z-10 bg-slate-300 top-0 sticky">
-    <ul class="flex items-center justify-end p-6 shadow-sm h-8">
+  <nav class="z-10 top-0 sticky">
+    <ul class="flex items-center justify-end p-6 shadow-sm h-8 backdrop-blur-lg backdrop-invert bg-slate-300/90">
       <NavLink href="#" text="Home" />
       <NavLink href="#bio" text="Bio" />
       <NavLink href="#pitch" text="Elevator Pitch" />
