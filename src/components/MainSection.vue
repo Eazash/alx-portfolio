d8d0ccea204bbbbc5d10ea2b68504ae0aac542e6
Ezira Ashenafi <eazash22@gmail.com>
2023-08-18 12:28:30 +0300

    fix: center main section text

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="px-2 py-8">
-    <div class="max-w-xl m-auto h-screen flex flex-col justify-center items-center">
+    <div class="max-w-xl m-auto h-screen flex flex-col justify-center items-center text-center">
       <h1 class="font-sans text-6xl md:text-8xl mb-2 text-blue-400">Ezira Ashenafi</h1>
       <p class="font-sans text-xl md:text-2xl ">Fullstack Developer, Devops Engineer, Cloud Engineer and Passionate
         Freelancer </p>
