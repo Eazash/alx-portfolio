464a5f7f10662a1ea2eec298489158195f06fd00
Ezira Ashenafi <eazash22@gmail.com>
2023-08-20 23:13:43 +0300

    feat: change image for main section

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,9 +1,13 @@
 <template>
   <section class="px-2 py-8">
-    <div class="max-w-xl m-auto h-screen flex flex-col justify-center items-center text-center">
-      <h1 class="font-sans text-4xl md:text-6xl mb-2 text-blue-400">Ezira Ashenafi</h1>
-      <p class="font-sans text-xl md:text-2xl ">Fullstack Developer, Devops Engineer, Cloud Engineer and Passionate
-        Freelancer </p>
+    <div class="h-screen flex items-center justify-center">
+      <div
+        class="max-w-xl h-64 backdrop:opacity-75 bg-black/50 m-auto flex flex-col justify-center items-center text-center border-2 rounded-md">
+        <h1 class="font-sans text-4xl md:text-6xl mb-2 text-blue-400">Ezira Ashenafi</h1>
+        <p class="font-sans text-xl md:text-2xl text-white">Fullstack Developer, Devops Engineer, Cloud Engineer and
+          Passionate
+          Freelancer </p>
+      </div>
     </div>
   </section>
 </template>
