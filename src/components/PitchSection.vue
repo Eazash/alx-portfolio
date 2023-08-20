44d1d29db8637f52a0eadcddfb6af11db3aadfc1
Ezira Ashenafi <eazash22@gmail.com>
2023-08-21 00:40:20 +0300

    feat: update pitch to use video

--- src/components/PitchSection.vue
+++ src/components/PitchSection.vue
@@ -5,11 +5,26 @@
   <section id="pitch" class="px-2 py-8">
     <SectionTitle title="My Elevator Pitch" />
     <div class="h-96 max-w-xl m-auto flex flex-col justify-center gap-4">
-      <div class="bg-slate-200 p-2 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam velit et
-        veniam
-        nostrum repudiandae quo fugit officia rerum, dolore quae impedit soluta repellendus sed quod corporis modi harum,
-        facere aliquid cumque aperiam, at quibusdam minus. Ducimus possimus, alias animi esse minus soluta? Reprehenderit
-        exercitationem quam vero autem. Id, ipsam sint!</div>
+      <div class="backdrop:opacity-30 bg-slate-200/50 p-8 rounded-md text-center">
+        <!-- <p class="text-justify">
+          Hello, my name is Ezira Ashenafi. I am a Software Engineering Graduate, Fullstack Typescript web developer and
+          DevOps Engineer with over three
+          years experience using tools such as VueJs, NestJS, Docker and more. I am currently growing my skills in AWS Cloud
+          engineering. I am
+          an energetic and curious individual, passionate
+          about
+          web development, who values quality. My extensive cross-domain experiences enable me to perceive and address
+          challenges earlier on, while enabling me to communicate with and lead diverse teams. I enjoy learning and
+          solving challenging problems, making me a versatile developer that
+          can quickly reach my best potential on any project. I am confident that I can be of great value for your Devops
+          and Fullstack
+          development needs.
+        </p> -->
+        <div style="position: relative; padding-bottom: 56.25%; height: 0;"><iframe
+            src="https://www.loom.com/embed/28333eccc31d4b1db84388e8a74bb3b8?sid=2714525d-7deb-4864-a5f6-93883d619938"
+            frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen
+            style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>
+      </div>
     </div>
   </section>
 </template>
